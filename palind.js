b62a67485fc92ae5b7c7d4ac0499cb2b62d09224
Bernard <bernardgodonou98@gmail.com>
2026-05-27 14:22:11 +0300

    modified version

--- palind.js
+++ palind.js
@@ -1,7 +1,9 @@
 function palindrome(s){
-    const reversedS = s.split('').reverse().join('');
-    return s.toLowerCase() === reversedS.toLowerCase();
-
-}
-
+    if(s.length<1000){
+        const reversedS = s.split('').reverse().join('');
+        return s.toLowerCase() === reversedS.toLowerCase();
+    }
+    
+    }
+ 
 console.log(palindrome('Bob'))
